fa93b485f9b06a4e574a27d147973095819a946c
EnverJ <Enlem1987@gmail.com>
2023-11-28 21:09:00 -0700

    2023 js training

---
+++ Chap-1-Entry/OPerators-7.js
@@ -0,0 +1,51 @@
+// Arithmetic operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+// Type Operators
+// Bitwise Operators
+
+// An operator is a symbol that usually represent an action or process
+// Arithmetic operators
+/*
+ + addition
+ - subtraction
+ * Multiplication
+ / division
+ % Modules (remainder of division)
+ ** Exponential
+ ++ increment
+*/
+
+// let x = 5;
+// let y = 2;
+// let result = x ** y; // x raise to the power of y
+// console.log("result is : " + result);
+// let a = 10;
+// //++a;  difference
+// let q = 2 * a++;
+// console.log("value of a is: " + a);
+// let b = 2 * ++a;
+// console.log("value of b is :" + b);
+
+// Assignment Operators
+/*
+  = Assign 
++= Add and assign
+-=  Sub and assign
+*=  Multiply and assign
+/=  Divide and assign
+%=  Modulus and assign
+**= Exponential and assign
+  */
+
+let x = 10;
+x = 10 + 20;
+
+x += 5; // x = x + 5
+console.log(x);
+
+// Comparison Operators
+// Logical Operators
+// Type Operators
+// Bitwise Operators
